b8577f00aefc3d58a9e08e7ae1f4eacc48852207
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-01 14:54:07 -0300

    Update general

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet"></link>;
 
 ReactDOM.render(
   <React.StrictMode>
