c24b75533edfdcddf8334f30ee5094d4447b1a78
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-06 19:14:28 -0300

    Componente que muestra un texto utilizando props

---
+++ src/components/card/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Card.scss'
+
+const Card = ({src, title, description, price,}) =>{
+    return(
+        <div className="card-item">
+            <img src={src} alt="Imagen Card"/>
+            <div className="card-body">
+            <p>{title}</p>
+            <p>{description}</p>
+            <p>{price}</p>
+            </div>
+            <button className="btn btn-danger">Comprar</button>
+        </div>
+    );  
+};
+
+export default Card
+
+
+
