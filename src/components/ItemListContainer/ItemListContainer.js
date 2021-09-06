2f0909bdaad15d663177ade49dd8bb7fc2b4456c
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-06 19:14:55 -0300

    Componente que muestra un texto utilizando props

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ItemListContainer.scss';
+
+const ItemListContainer = (props) =>{
+    return(
+        <h1>¡Bienvenidos a Hardware Place!{props.name}</h1>
+    )
+}
+
+export default ItemListContainer
