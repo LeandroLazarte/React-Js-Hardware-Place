21aca25426620885bbbadfa8e9051a71529d6a43
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-06 19:17:43 -0300

    Upgrade general

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React from 'react'
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
+import NavBar from './components/navbar/NavBar';
+import Card from './components/card/Card';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 const App = () =>{
-
-const myFunction = () =>{
-  alert('¡Bienvenidos a Hardware Place!');
-};
-
-let age = 18;
-
   return (
     <div>
     <NavBar/>
-    <div className="first-div">
-  <h1 className="title">¡Bienvenidos a Hardware Place!</h1>
-  <div>
-    <p className="paragraph">Nombre: Leandro</p>
-    <p className="paragraph">Apellido: Lazarte</p>
-    <p className="paragraph">Edad: {2 + 24}</p>
-    {age>= 18 ? <p className="paragraph">Es Mayor de edad</p>: <p className="paragraph">Es menor de edad</p>}
-  </div>
-  <button className="btn btn-danger" id="button" onClick={myFunction}>Click Here</button>
-  </div>
+    <ItemListContainer/>
+    <div className ="card" style={{display:"flex"}}>
+    <Card 
+      src="./assets/monitorGigabyte.webp"
+      title="Monitor Gigabyte 240Hz"
+      description="Gigabyte 240Hz 0.5ms"
+      price= "$117.950"
+    />
+    <Card
+    src="./assets/monitorLG.webp"
+    title="Monitor LG 144Hz"
+    description="LG 144Hz 1ms"
+    price= "$85.290"
+    />
+    <Card
+    src="./assets/monitorSamsung.webp"
+    title="Monitor Samsung 60Hz"
+    description="Samsung 60Hz 5ms"
+    price= "$28.900"
+    />
+    <Card
+    src="./assets/graficaEVGA.webp"
+    title="Gráfica EVGA"
+    description="EVGA RTX 3090 24Gb"
+    price= "$320900"
+    />
+    <Card
+    src="./assets/graficaMSI.webp"
+    title="Gráfica MSI"
+    description="MSI RTX 3080 10Gb"
+    price= "$250000"
+    />
+    <Card
+    src="./assets/graficaGigabyte.webp"
+    title="Gráfica Gigabyte"
+    description="Gigabyte RTX 3070 8Gb"
+    price= "$190890"
+    />
+    </div>
   </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet"></link>;
