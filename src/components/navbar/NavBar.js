7673af447f876df38f5119100c08efeec7973ae2
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-22 23:44:02 -0300

    Se incorpo el LINK para navegar en el sitio

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,39 +1,60 @@
-import React from 'react';
-import  {Navbar, Nav } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import "./NavBar.scss";
-import logo from './Logo.png';
-import CartWidget from '../cartWidget/CartWidget';
+import logo from "./Logo.png";
+import CartWidget from "../cartWidget/CartWidget";
+import { Link } from "react-router-dom";
 
-const Header = () =>{
-const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-const handelClick = () =>{
-  if (isLoggedIn === true) {
-    setIsLoggedIn(false)
-  } else{
-    setIsLoggedIn(true)
-  }
-}
-        return( <Navbar className="NavBar" bg="dark" variant={"dark"} expand="xxl">
-        <Navbar.Brand href="#"><img src={logo} className="logo" alt="Logo Empresarial"/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mr-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link className="link" href="#action1">Inicio</Nav.Link>
-            <Nav.Link className="link" href="#action2">Productos</Nav.Link>
-            <Nav.Link className="link" href="#action3">Nosotros</Nav.Link>
-            <Nav.Link className="link" href="#action4">Contacto</Nav.Link>
-            <Nav.Link className="link" href="#action5">Registro</Nav.Link>
-            {isLoggedIn.toString()}
-            <button onClick={handelClick}>Cambiar estado</button>
-          </Nav>
-        </Navbar.Collapse>
-        <CartWidget/>
-      </Navbar>)
-}
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const handelClick = () => {
+    if (isLoggedIn === true) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  };
+  return (
+    <Navbar className="NavBar" bg="dark" variant={"dark"} expand="xxl">
+      <Link to="/">
+        <Navbar.Brand href="#">
+          <img src={logo} className="logo" alt="Logo Empresarial" />
+        </Navbar.Brand>
+      </Link>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="mr-auto my-2 my-lg-0"
+          style={{ maxHeight: "100px" }}
+          navbarScroll
+        >
+          <Link to="/">
+            <Nav.Link className="link" href="#action1">
+              Inicio
+            </Nav.Link>
+          </Link>
+          <Link to="/nosotros">
+            <Nav.Link className="link" href="#action3">
+              Nosotros
+            </Nav.Link>
+          </Link>
+          <Link to="/contacto">
+            <Nav.Link className="link" href="#action4">
+              Contacto
+            </Nav.Link>
+          </Link>
+          <Link to="/registro">
+            <Nav.Link className="link" href="#action5">
+              Registro
+            </Nav.Link>
+          </Link>
+          {isLoggedIn.toString()}
+          <button onClick={handelClick}>Cambiar estado</button>
+        </Nav>
+      </Navbar.Collapse>
+      <CartWidget />
+    </Navbar>
+  );
+};
 
-
-export default Header
+export default Header;
