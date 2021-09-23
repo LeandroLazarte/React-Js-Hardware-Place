de193e1e3492d4d5e1b1f9aa7781d6fb3cc56f4a
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-22 23:39:21 -0300

    Se incorporo la estructura de rutas para navegar entre las diferentes paginas del sitio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/navbar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import React from "react";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/navbar/NavBar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import DescriptionProduct from "./pages/DescriptionProduct";
+import Home from "./pages/Home";
+import Nosotros from "./pages/Nosotros";
+import Contacto from "./pages/Contacto";
+import Registro from "./pages/Registro";
 
-const App = () =>{
+const App = () => {
   return (
-    <div className="App">
-    <NavBar/>
-    <ItemListContainer/>
-  </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/product/:id" component={DescriptionProduct} />
+        <Route path="/nosotros" component={Nosotros} />
+        <Route path="/contacto" component={Contacto} />
+        <Route path="/registro" component={Registro} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
