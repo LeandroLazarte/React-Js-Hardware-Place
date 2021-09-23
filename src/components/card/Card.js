7ca6e20d992637683d7a1ad003fa2fc4af11dd59
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-22 23:42:06 -0300

    Se importo el link para mostrar la descripcion de cada producto

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import ItemCount from '../ItemCount/itemCount';
-import './Card.scss'
+import React from "react";
+import ItemCount from "../ItemCount/itemCount";
+import "./Card.scss";
+import { Link } from "react-router-dom";
 
 const Click = () => {
-    alert("Se ha añadido su/s producto al carrito");
-}
-
-const Card = ({id,image, title, description, price,stock}) =>{
-    return(
-        <div key={id} className="card-item">
-            <img src={image} className="img-product" alt="Imagen Card"/>
-            <div className="card-body">
-            <p>{title}</p>
-            <p>Descripción</p>
-            <p>{description}</p>
-            <p>{price}</p>
-            <p>Cantidad</p>
-            <p className="count"><ItemCount/></p>
-            </div>
-            <button onClick={Click} className="btn btn-danger">Añadir al carrito</button>
-        </div>
-    );  
+  alert("Se ha añadido un producto al carrito");
 };
 
-export default Card
+const Card = ({ id, image, title, description, price, productId}) => {
+  return (
+    <div key={id} className="card-item">
+      <Link to={`/product/${productId}`}>
+        <img src={image} className="img-product" alt={title} />
+      </Link>
+      <div className="card-body">
+        <p>{title}</p>
+        <p>Descripción</p>
+        <p>{description}</p>
+        <p>{price}</p>
+        <p>Cantidad</p>
+        <p className="count">
+          <ItemCount />
+        </p>
+      </div>
+      <button onClick={Click} className="btn btn-danger">
+        Añadir al carrito
+      </button>
+    </div>
+  );
+};
 
-
-
+export default Card;
