91bdaaca1c7329a164a6d7bd22eb767fabeed1f8
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-09-22 23:44:31 -0300

    Modificaciones generales

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./ItemListContainer.scss";
-import Card from "../card/Card"
+import Card from "../card/Card";
 
 const ItemListContainer = () => {
   const [data, setData] = React.useState([]);
-//   const [loading, setLoad] = React.useState(false);
+  //   const [loading, setLoad] = React.useState(false);
 
   React.useEffect(() => {
-    const url ="https://fakestoreapi.com/products?limit=6";
-    
+    const url = "https://fakestoreapi.com/products?limit=6";
+
     fetch(url)
       .then((response) => {
         if (response.ok) {
@@ -22,26 +22,27 @@ const ItemListContainer = () => {
         console.log(`Hubo un error inesperado ${error.status}`)
       );
   }, []);
-  
+
   console.log(data);
 
-//   const buyProduct = (product) => {
-//       console.log(`Se ha añadido al carrito ${product}` );
-//   }
+  //   const buyProduct = (product) => {
+  //       console.log(`Se ha añadido al carrito ${product}` );
+  //   }
 
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-    {data.map((product) => {
-        return(
-              <Card
-                key={product.id}
-                image={product.image}
-                title={product.title}
-                description={product.description}
-                category={product.category}
-            />
+      {data.map((product) => {
+        return (
+          <Card
+            key={product.id}
+            productId={product.id}
+            image={product.image}
+            title={product.title}
+            description={product.description}
+            category={product.category}
+          />
         );
-    })}
+      })}
     </div>
   );
 };
