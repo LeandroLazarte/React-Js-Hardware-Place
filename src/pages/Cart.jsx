1cb95b1949dd9e89ecaf19356b8b59e15fa65fc0
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-10-04 23:49:20 -0300

    En este componente se puede ver los detalles de la compra con el precio total de los productos

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useCart } from '../components/context/CartContext';
+import { useHistory } from 'react-router';
+import { Row, Card } from 'react-bootstrap';
 
-const Cart = () => {
-    return(
-        <div>
-        <h1>Cart</h1>
-        <button type="button" className="btn btn-danger">Finalizar Compra</button>
-        </div>
+const Carrito = () => {
+    const { cart, finallyPrice } = useCart();
+    let history = useHistory();
+
+    // const handleDelete = (id) => {
+    //     removeItem(id);
+    // }
+
+    useEffect(() => {
+        if (cart.length === 0) {
+            history.push("/");
+        }
+    }, [cart, history]);
+
+    // const handleClear = (key) => {
+    //     clear();
+    // }
+    return (
+        <Row>
+            {cart.map((product) => (
+                <Card width={300} height={300}>
+                    <div key={product.id} className="content">
+                        <h1>{product?.title}</h1>
+                        <img style={{ width: '300px', height: '300px' }} className="image" src={product?.image} alt="producto" />
+                        <p style={{ fontSize: '20px' }} className="description"> Detalle: {product?.description}</p>
+                        <p className="description">Precio: ${product?.price}</p>
+                        <p className="description">Cantidad:{product?.cantidad}</p>
+                    </div>
+                </Card>
+            ))
+            }
+            <div style={{ marginBottom:40}}>
+            <h1>{`Total de la compra: $${finallyPrice().toFixed(2)}`}</h1>
+            </div>
+        </Row>
+
+
     )
 }
 
-export default Cart;
+export default Carrito;
