256d8fb629f22efa55b936304da2d23920871aaa
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-10-26 15:51:32 -0300

    Aqui se encuentra la pagina principal del sitio

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ItemListContainer from '../../components/ItemListContainer/ItemListContainer';
 import CarouselImages from "../../components/carouselImages/CarouselImages";
+import Banner from "../../components/bannerMarcas/Banners";
 import Footer from "../../components/footer/Footer";
 
 const styles = {color: 'orange' , textDecoration: 'underline', textAlign: 'center'};
@@ -11,6 +12,7 @@ const Home = () => {
             <CarouselImages/>
             <h1 style={styles}>Productos Destacados</h1>
             <ItemListContainer />
+            <Banner />
             <Footer />
         </div>
     )
