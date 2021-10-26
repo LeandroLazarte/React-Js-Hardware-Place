140121832abb42ef914835731329b25e0210eb79
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-10-26 04:54:56 -0300

    Barra de navegacion para poder desplazarse por todo el sitio

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,14 +3,14 @@ import { Navbar, Nav, Badge } from "react-bootstrap";
 import "./NavBar.scss";
 import logo from "./Logo.png";
 import { Link } from "react-router-dom";
-import { useCart } from "../context/CartContext";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import { UseCart } from "../context/CartContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  const {cart } = useCart();
+  const { cart } = UseCart();
   return (
-    <Navbar className="NavBar" bg="dark" variant={"dark"} expand="xxl">
+    <Navbar className="NavBar" bg="dark" variant={"primary"} expand="xxl">
       <Link to="/">
         <Navbar.Brand href="#">
           <img src={logo} className="logo" alt="Logo Empresarial" />
@@ -38,16 +38,11 @@ const Header = () => {
               Contacto
             </Nav.Link>
           </Link>
-          <Link to="/registro">
-            <Nav.Link className="link" href="#action5">
-              Registro
-            </Nav.Link>
-          </Link>
           <Link to="/cart">
             <Nav.Link className="link" href="#action6">
-            <Badge bg="secondary">{cart ? cart :0}</Badge>
-            <FontAwesomeIcon icon={faShoppingCart}/>
-            Carrito
+              Carrito
+              <FontAwesomeIcon icon={faShoppingCart} />
+              <Badge bg="secondary">{cart.length}</Badge>
             </Nav.Link>
           </Link>
         </Nav>
