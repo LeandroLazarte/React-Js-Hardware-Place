16348df5e3c5f825937c0c40479a6e728928519c
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-10-26 04:57:54 -0300

    Aqui se encuentra todo el esqueleto del sitio

--- src/App.js
+++ src/App.js
@@ -9,24 +9,22 @@ import DescriptionProduct from "./pages/details/DescriptionProduct";
 import Home from "./pages/Home/Home";
 import Nosotros from "./pages/Nosotros/Nosotros";
 import Contacto from "./pages/Social/Contacto";
-import Registro from "./pages/Registro/Registro";
 import Cart from "./pages/Cart/Cart";
 
 function App() {
   return (
-    <ShoppingContext>
     <BrowserRouter>
+    <ShoppingContext>
         <NavBar />
         <Switch>
           <Route path="/cart" component={Cart} />
           <Route path="/product/:id" component={DescriptionProduct} />
           <Route path="/nosotros" component={Nosotros} />
           <Route path="/contacto" component={Contacto} />
-          <Route path="/registro" component={Registro} />
           <Route path="/" component={Home} />
         </Switch>
+        </ShoppingContext>
     </BrowserRouter>
-    </ShoppingContext>
   );
 }
 
