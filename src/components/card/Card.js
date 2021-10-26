8458d903abe068cefe06e5d22276d9792ac38a8a
Leandro Lazarte <leandro.lazarte27@hotmail.com>
2021-10-26 15:48:25 -0300

    Se puede ver los productos que cuenta la empresa para la venta

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,31 @@
 import React from "react";
 import "./Card.scss";
 import { Link } from "react-router-dom";
+import { CardGroup, Card } from "react-bootstrap";
 
-const Card = ({ id, category, image, title, description, productId }) => {
+const Cards = ({ id, category, image, title, description, productId }) => {
   return (
-    <div key={id} className="card ">
-      <img src={image} className="img-product" alt={title} />
-      <div className="card-body">
-        <p>{title}</p>
-        <p>{category}</p>
-        <div>
-          <p>Descripción: {description}</p>
-        </div>
-      </div>
-      <Link to={`/product/${productId}`}>
-        <button id="button" className="btn btn-danger">Ver Producto</button>
-      </Link>
-    </div>
+    <CardGroup >
+      <Card style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'space-between'}} key={id}>
+        <Card.Img
+          style={{ width: "250px", height: "250px" }}
+          variant="top"
+          src={image}
+          alt={title}
+        />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{category}</Card.Text>
+          <Card.Text>{description}</Card.Text>
+          <Link to={`/product/${productId}`}>
+            <button id="button" className="btn btn-danger">
+              Ver Producto
+            </button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </CardGroup>
   );
 };
 
-export default Card;
+export default Cards;
